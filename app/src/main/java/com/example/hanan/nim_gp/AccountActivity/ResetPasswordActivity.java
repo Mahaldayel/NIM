afa376bbfba87ed5346bf4a110d9aa76a2bc5be8
ArwaHamed <32874102+ArwaHamed@users.noreply.github.com>
2019-01-24 00:12:58 +0300

    arwa

---
+++ app/src/main/java/com/example/hanan/nim_gp/AccountActivity/ResetPasswordActivity.java
@@ -0,0 +1,15 @@
+package com.example.hanan.nim_gp.AccountActivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.hanan.nim_gp.R;
+
+public class ResetPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_password);
+    }
+}
---
+++ app/src/main/java/com/example/hanan/nim_gp/AccountActivity/SignUpActivity.java
@@ -0,0 +1,14 @@
+package com.example.hanan.nim_gp.AccountActivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.hanan.nim_gp.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
