14f649a2b79225bff069141e7fd2681e0fad7502
ArwaHamed <32874102+ArwaHamed@users.noreply.github.com>
2019-03-21 07:40:19 +0300

    Arwa

--- app/src/main/java/com/example/hanan/nim_gp/Game/LevelsOfTheGame.java
+++ app/src/main/java/com/example/hanan/nim_gp/Game/LevelsOfTheGame.java
@@ -2,16 +2,63 @@ package com.example.hanan.nim_gp.Game;
 
 public class LevelsOfTheGame {
 
-
-    /*level one **/
-    public final int LEVEL_NUMBER_ONE = 1;
-    public int LEVEL_SCORE_ONE = 0 ;
-    public final String LEVEL_NAME_ONE = "Level one";
-    public final double OPTIMAL_TIME_ONE = 0;
-    public final double MAX_TIME_ONE = 0;
+    private int levelNum;
+    private int levelScore;
+    private String levelName;
+    private double levelOptimalTime;
+    private double levelMaxTime;
 
 
+    public LevelsOfTheGame( int levelNum,int levelScore, String levelName, double levelOptimalTime, double levelMaxTime){
+        this.levelNum = levelNum;
+        this.levelScore = levelScore;
+        this.levelName = levelName;
+        this.levelOptimalTime = levelOptimalTime;
+        this.levelMaxTime = levelMaxTime;
     }
+
+    public void setLevelNum(int levelNum) {
+        this.levelNum = levelNum;
+    }
+
+    public void setLevelScore(int levelScore) {
+        this.levelScore = levelScore;
+    }
+
+    public void setLevelName(String levelName) {
+        this.levelName = levelName;
+    }
+
+    public void setLevelOptimalTime(double levelOptimalTime) {
+        this.levelOptimalTime = levelOptimalTime;
+    }
+
+    public void setLevelMaxTime(double levelMaxTime) {
+        this.levelMaxTime = levelMaxTime;
+    }
+
+    public int getLevelNum() {
+        return levelNum;
+    }
+
+    public int getLevelScore() {
+        return levelScore;
+    }
+
+    public String getLevelName() {
+        return levelName;
+    }
+
+    public double getLevelOptimalTime() {
+        return levelOptimalTime;
+    }
+
+    public double getLevelMaxTime() {
+        return levelMaxTime;
+    }
+
+
+}
 
 
 
